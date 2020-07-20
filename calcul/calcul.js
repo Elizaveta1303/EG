6dd6d9066f3cad77c2b1624a0a39f497bdf9424e
Elizaveta1303 <lizagor130324@gmail.com>
2020-07-20 13:57:21 +0300

    first commit

--- calcul/calcul.js
+++ calcul/calcul.js
@@ -1,38 +1,42 @@
 function calculate(){
     var bill_count = document.getElementById('bill_count').value;
-    var service_quality = document.getElementById('service_quality').value;
-    var people_num = document.getElementById('people_num').value;
+    var service_quality = document.getElementById("service_quality").value;
+    var people_num = document.getElementById("people_num").value;
+    var currency = document.getElementById("currency").value;
 
-    //simple validation
-    if (!bill_count || !service_quality){
-        alert('Please enter values!');
-        return;
-    }
 
-    //check people input value
-    if(!people_num || people_num <=1){
-        people_num=1;
-    }   else {
-        document.getElementById('each').style.display = 'block';
-    }
-
-    //Calculate tips
-    var total = (bill_count = service_quality) / people_num;
+if (!bill_count||bill_count==0){
+    document.getElementById('bill_count').style.borderColor='red';
+} else {
+    document.getElementById('bill_count').style.borderColor='black' 
+}
+ if(!people_num||people_num<=0){
+     people_num=1;
+     document.getElementById("people_num").value=people_num;
+ } else{
+     document.getElementById("each").style.display ="block";
+ }
+ 
+ switch (currency) {
+    case ("₽"): currency = 0.014; break;
+    case ("$"): currency = 1; break;
+    case ("€"): currency = 1.14; break;
+}
+    var total = (bill_count * service_quality*currency) / people_num;
     
-
-    //round value
-    total = Math.round(total * 100) / 100;
+    total=Math.round(total*100)/100;
     total = total.toFixed(2);
-
-    document.getElementById("totalTip").style.display = "block";
-    document.getElementById("tip").innerHTML = total;
-
+ 
+    document.getElementById("totalTip").style.display="block";
+    document.getElementById('tip').innerHTML=total;
 }
 
-//Hide tips element on load
-document.getElementById("totalTip").style.display = "none";
-document.getElementById("each").style.display = "none";
 
-document.getElementById("calculate").onclick = function(){
+//hide tip element
+document.getElementById("totalTip").style.display='none';
+document.getElementById("each").style.display='none';
+
+document.getElementById("calculate").onclick= function(){
     calculate();
+    console.log("работаю");
 }
